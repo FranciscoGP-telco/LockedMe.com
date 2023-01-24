985cf855925e6354b585554ddca2749800f3c271
Francisco García <franciscogarciapozo@gmail.com>
2023-01-24 20:01:39 +0100

    added documents to upload and solved a small bug in the menu

--- src/com/lockedme.java
+++ src/com/lockedme.java
@@ -5,7 +5,6 @@ public class lockedme {
 		//From the main app, we launch the three functions from the menu
 		//To show the welcome message, the main menu options, and the function to navigate the menu
 		Menu.showWelcome();
-		Menu.showMainMenu();
 		Menu.navigationMainMenu();
 	}
 }
